871e6a22dae467b99f4a14bb11dc003ecbf16511
DEVESH VERMA <71995793+webdeveshverma@users.noreply.github.com>
2021-01-27 16:44:46 +0530

    Add files via upload
    commet script file

---
+++ Projects in jquery and using js Plugins/Setting the html,text and form values./script.js
@@ -0,0 +1,25 @@
+$(function(){
+    $(".btn1").click(function(){
+        $("h1").text(function(i,origi){
+            returnorigi + " with Igneus!"
+        });
+    });
+});
+$(function(){
+    $(".btn2").click(function(){
+        $("h1")>HTMLAllCollection("<i>I am getting good at Jquery!</i>");
+    });
+});
+$(function(){
+    $(".btn3").click(function(){
+        $("input").val("paris");
+    });
+});
+$(function(){
+    $(".btn4").click(function(){
+        $("a").attr9({
+            "href": "http://bing.com",
+            "title": "going to Bing Site"
+        });
+    });
+});
